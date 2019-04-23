a214b6f447ed55a90509e09d4244d4cd484fc610
Tony Cheung <iamtonycheung@gmail.com>
2019-04-24 01:16:03 +0800

    amend sharpe formula

--- find_stock_sharpe_by_yahoo_fin.py
+++ find_stock_sharpe_by_yahoo_fin.py
@@ -11,19 +11,19 @@ def find_stock_sharpe_by_yahoo_fin(code):
 
 	df=get_data(code)
 	df=df.drop(df.columns[[0,1,2,3,5,6]], axis=1)
+	df['daily_return'] = df['adjclose'].pct_change()
 	df.sort_index(inplace=True,ascending=False)
-	stock_returns = df.pct_change()
-	stock_mean_return = stock_returns.mean()
-	stock_return_stdev = stock_returns.std()
-	stock_annualised_return = round(stock_mean_return * 252,2)
-	stock_annualised_stdev = round(stock_return_stdev * np.sqrt(252),2)
-	stock_sharpe_ratio=(stock_mean_return/stock_return_stdev)
-	stock_sharpe_ratio=(252**0.5)*stock_sharpe_ratio
+	stock_mean_return = df['daily_return'].mean()
+	stock_return_stdev =df['daily_return'].std()
+
+	stock_annualised_return = round(252*stock_mean_return,2)
+	stock_annualised_stdev = round(np.sqrt(252)*stock_return_stdev,2)
+	stock_sharpe_ratio=round(np.sqrt(252)*stock_mean_return/stock_return_stdev,2)
 
 	myStock=Stock()
 	myStock.code=code
-	myStock.stock_annualised_return=format(stock_annualised_return.values)
-	myStock.stock_return_stdev=format(stock_annualised_stdev.values)
-	myStock.stock_sharpe_ratio=format(stock_sharpe_ratio.values)
+	myStock.stock_annualised_return=stock_annualised_return
+	myStock.stock_return_stdev=stock_annualised_stdev
+	myStock.stock_sharpe_ratio=stock_sharpe_ratio
 	
 	return myStock
